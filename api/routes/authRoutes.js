fb334e4bd4305c2fd539a6985910d48d006c2d28
Thanh Vinh Tong <vinhtongthanh57@gmail.com>
2025-03-29 05:15:23 +0800

    Fix bug in calling protectRoute from authRoutes.js

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { signin, signup, signout } from '../controllers/authController.js';
+import { protectRoute } from '../middleware/auth.js';
 
 const router = express.Router();
 
