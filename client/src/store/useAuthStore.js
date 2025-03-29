bc359f5f64cefc44df402603f76e26dd2012c248
Thanh Vinh Tong <vinhtongthanh57@gmail.com>
2025-03-30 02:37:49 +0800

    Update Match, Conversation functionality

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -1,58 +1,61 @@
 import { create } from "zustand";
-import { axiosInstance } from "../lib/axios.js";
+import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
+import { disconnectSocket, initializeSocket } from "../socket/socket.client";
 
 export const useAuthStore = create((set) => ({
-    authUser:null,
-    checkingAuth:true,
-    loading:false,
+	authUser: null,
+	checkingAuth: true,
+	loading: false,
 
-    signup: async(signupData) => {
-        try {
-            set({loading: true});
-            const res = await axiosInstance.post("/auth/signup", signupData);
-            set({authUser: res.data.user});
-            toast.success("Account created successfully");
-        } catch (error) {
-            toast.error(error.response.data.message || "Something went wrong");
-        } finally {
-            set({loading: false});
-        }
-    },
-    
-    signin: async(signinData) => {
-        try {
-            set({ loading: true });
-            const res = await axiosInstance.post("/auth/signin", signinData);
-            set({ authUser: res.data.user });
-            toast.success("Signed in successfully");
-        } catch (error) {
-            toast.error(error.response.data.message || "Something went wrong");
-        } finally {
-            set({loading: false});
-        }
-    },
+	signup: async (signupData) => {
+		try {
+			set({ loading: true });
+			const res = await axiosInstance.post("/auth/signup", signupData);
+			set({ authUser: res.data.user });
+			initializeSocket(res.data.user._id);
 
-    signout: async () => {
-        try {
-            const res = await axiosInstance.post("/auth/post");
-            if (res.status === 200) set({ authUser: null });
-        } catch (error) {
-            toast.error(error.response.data.message || "Something went wrong");
-        }
-    },
+			toast.success("Account created successfully");
+		} catch (error) {
+			toast.error(error.response.data.message || "Something went wrong");
+		} finally {
+			set({ loading: false });
+		}
+	},
+	signin: async (signinData) => {
+		try {
+			set({ loading: true });
+			const res = await axiosInstance.post("/auth/signin", signinData);
+			set({ authUser: res.data.user });
+			initializeSocket(res.data.user._id);
+			toast.success("Signed in successfully");
+		} catch (error) {
+			toast.error(error.response.data.message || "Something went wrong");
+		} finally {
+			set({ loading: false });
+		}
+	},
+	signout: async () => {
+		try {
+			const res = await axiosInstance.post("/auth/signout");
+			disconnectSocket();
+			if (res.status === 200) set({ authUser: null });
+		} catch (error) {
+			toast.error(error.response.data.message || "Something went wrong");
+		}
+	},
+	checkAuth: async () => {
+		try {
+			const res = await axiosInstance.get("/auth/me");
+			initializeSocket(res.data.user._id);
+			set({ authUser: res.data.user });
+		} catch (error) {
+			set({ authUser: null });
+			console.log(error);
+		} finally {
+			set({ checkingAuth: false });
+		}
+	},
 
-    checkAuth: async () => {
-        try {
-            const res = await axiosInstance.get("/auth/me");
-            set({authUser: res.data.user});
-        } catch (error) {
-            // set({ authUser: null });
-            console.log(error);
-        } finally {
-            set({ checkingAuth: false });
-        }
-    },
-
-    setAuthUser: (user) => set({ authUser: user }),
-}))
+	setAuthUser: (user) => set({ authUser: user }),
+}));
