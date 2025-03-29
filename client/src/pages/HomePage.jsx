850375b275b025ea040444949784390f030a3634
Thanh Vinh Tong <vinhtongthanh57@gmail.com>
2025-03-29 19:07:52 +0800

    Add Authentication functionalities and Pages

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useAuthStore } from '../store/useAuthStore.js'
 
 const HomePage = () => {
+  const { signout } = useAuthStore();
+
   return (
-    <div>HomePage</div>
+    <div>
+      HomePage
+      <button onClick={signout}>SignOut</button>
+    </div>
   )
 }
 
